b7f43f3ceaaca234d8142de1d32a7831a7338b59
tusharpatel0504 <tushar789patel@gmail.com>
2025-08-28 01:56:40 +0530

    hello

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,17 +1,11 @@
 /** @type {import('tailwindcss').Config} */
-
-import tailwindScrollbar from 'tailwind-scrollbar';
 export default {
   content: [
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {
-      fontFamily:{
-        'sans': ['Inter', 'sans-serif'],
-      }
-    },
+    extend: {},
   },
-  plugins: [tailwindScrollbar],
+  plugins: [],
 }
